86f50ebadbff0bbef2a79f46fa4130bc594f14d8
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-22 14:02:57 -0600

    Sample images updated

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 const Image = styled.div`
-  width: 400px;
-  height: 400px;
+  width: 305px;
+  height: 305px;
   background: no-repeat center/150% url(/img/${({ index }) => index}.jpg);
 `;
 
