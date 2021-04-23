de9d33c908c0273f2314f183c144c5075a10059b
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-23 02:14:04 -0600

    cascade layout added to gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,5 +1,5 @@
-import { useLocation, Link } from "react-router-dom";
-import styled from "styled-components";
+import { useLocation, Link, useRouteMatch } from "react-router-dom";
+import styled, { css } from "styled-components";
 import LazyLoad from "react-lazyload";
 
 //data
@@ -10,32 +10,81 @@ import Image from "../components/Image";
 import { UserGrid } from "../components/UserGrid";
 
 const PhotoGrid = styled.div`
-  width: 950px;
-  margin: auto;
   display: grid;
   grid-template-columns: repeat(3, 305px);
+  justify-content: center;
+  grid-auto-rows: 305px;
+  gap: 15px;
+  ${({ cascade }) =>
+    cascade &&
+    css`
+      grid-auto-rows: 200px;
+      grid-gap: 5px;
+    `}
+`;
+
+const LinkGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: center;
   gap: 20px;
+  margin-bottom: 20px;
+`;
+
+const TabLinkStyled = styled(Link)`
+  text-decoration: none;
+  color: #ccc;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  ${({ selected }) => selected && "color: black"}
+`;
+
+const ImageLink = styled(Link)`
+  background: no-repeat center/150% url(/img/${({ index }) => index}.jpg);
+  background-size: cover;
+  :hover {
+    opacity: 0.7;
+  }
+  ${({ cascade }) =>
+    cascade &&
+    css`
+      &:nth-of-type(2n) {
+        grid-row-start: span 2;
+      }
+    `}
 `;
 
 const Gallery = () => {
-  let location = useLocation();
+  const location = useLocation();
+  const { url } = useRouteMatch();
+
+  const galleryCascade = location.search === "?type=cascade";
 
   return (
     <div>
       <UserGrid />
-      <PhotoGrid>
+      <LinkGridStyled>
+        <TabLinkStyled selected={!galleryCascade} to={`${url}`}>
+          square
+        </TabLinkStyled>
+        <TabLinkStyled
+          selected={galleryCascade}
+          to={{ pathname: `${url}`, search: "?type=cascade" }}
+        >
+          cascade
+        </TabLinkStyled>
+      </LinkGridStyled>
+      <PhotoGrid cascade={galleryCascade}>
         {IMAGES.map((i) => (
-          <Link
+          <ImageLink
+            cascade={galleryCascade}
             key={i.id}
             to={{
               pathname: `/img/${i.id}`,
               state: { background: location },
             }}
-          >
-            <LazyLoad height={305}>
-              <Image index={i.id} />
-            </LazyLoad>
-          </Link>
+            index={i.id}
+          />
         ))}
       </PhotoGrid>
     </div>
