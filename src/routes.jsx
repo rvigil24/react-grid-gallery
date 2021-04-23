9860eb98115eacadfbb3f98c0e4b073d5123524c
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-23 03:18:21 -0600

    routes updated

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 
 //pages
-import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
-import ImageView from "./pages/ImageView";
 import Modal from "./components/Modal";
 
 const Routes = () => {
@@ -14,9 +12,7 @@ const Routes = () => {
   return (
     <>
       <Switch location={background || location}>
-        <Route exact path="/" children={<Home />} />
-        <Route path="/gallery" children={<Gallery />} />
-        <Route path="/img/:id" children={<ImageView />} />
+        <Route path="/" children={<Gallery />} />
       </Switch>
 
       {background && <Route path="/img/:id" children={<Modal />} />}
